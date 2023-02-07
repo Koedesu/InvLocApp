ef677528a9c0603d63e69b46533727d7f55965ab
Koedesu <marco.valadez@uttn.mx>
2023-02-07 12:18:08 -0600

    ps disque jalaba

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 include "inc/header.php";
 if(isset($_SESSION['administrador']))
-    $pagina=(isset($_GET['pagina'])) ? $_GET['pagina'] : "index";
+    $pagina=(isset($_GET['pagina'])) ? $_GET['pagina'] : "pages/index.php";
 else
     $pagina="login";
 include "pages/".$pagina.".php";
--- procesos/login.php
+++ procesos/login.php
@@ -1,17 +1,22 @@
 <?php
+session_start();
+require "../config.php";
+$msg = "No se pudo acceder";
 if(isset($_POST['btnLogin'])):
-    require "class/conexion.php";
-    $user = new InvLocAppDB();
-
+    require "../class/conexion.php";
+    $user= new InvLocAppDB();
+    
     $email = $_POST['txtEmail'];
     $password = md5($_POST['txtPassword']);
 
-    $data = "email'".$email."',password='".$password."'";
+    $data = "email='".$email."'AND password='".$password."'";
     $u=$user->buscar("login",$data);
-    if($u):   
-    foreach($u as $data):
-        echo $data ['email']."ok";
-    endforeach;
-else:
-    echo "No hay acceso";
+    if($u):
+        foreach($u as $data):
+            $_SESSION['administrador'] = $data['nombre'];
+            $msg="Bienvenido";
+        endforeach;
+    endif;
 endif;
+header("location:".urlsite."?mensaje=".$msg);
+?>
