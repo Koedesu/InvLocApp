e1822f3a7ef5946428fad085ea0905132bc1de84
Koedesu <marco.valadez@uttn.mx>
2023-02-22 22:50:27 -0600

    ya jala esta mamada

--- procesos/login.php
+++ procesos/login.php
@@ -1,22 +1,21 @@
 <?php
 session_start();
 require "../config.php";
-$msg = "No se pudo acceder";
+$msg="No se pudo acceder.";
 if(isset($_POST['btnLogin'])):
     require "../class/conexion.php";
-    $user= new InvLocAppDB();
-    
-    $email = $_POST['txtEmail'];
-    $password = md5($_POST['txtPassword']);
+    $user       =  new InvLocAppDB();
 
-    $data = "email='".$email."'AND password='".$password."'";
+    $email      =   $_POST['txtEmail'];
+    $password   =   md5($_POST['txtPassword']);
+
+    $data       =   "email='".$email."'AND password='".$password."'";
     $u=$user->buscar("login",$data);
     if($u):
         foreach($u as $data):
-            $_SESSION['administrador'] = $data['nombre'];
-            $msg="Bienvenido";
+            $_SESSION['administrador'] = $data['id'];
+            $msg = $data['nombre '];
         endforeach;
     endif;
 endif;
 header("location:".urlsite."?mensaje=".$msg);
-?>
